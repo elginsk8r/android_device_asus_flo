770338afd19c3ed225b2ef7c096d1d515cb2fd70
Nick Reuter <nreuter85@gmail.com>
2014-03-31 08:38:42 -0500

    flox: evervolv setup
    Change-Id: Iae24a685c7cc1edf10ee5dcd7a31608f635e38c2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_flox.mk
+	$(LOCAL_DIR)/ev_flox.mk
--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,7 +31,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=flox user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += vmalloc=340M
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
-TARGET_KERNEL_SOURCE := kernel/google/msm
+TARGET_KERNEL_SOURCE := kernel/asus/flo
 TARGET_KERNEL_CONFIG := lineageos_flo_defconfig
 
 # QCOM Hardware
