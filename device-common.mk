71ee13d01da732f5df394bda3266c0075bbb6bd8
Ying Wang <wangying@google.com>
2014-12-10 12:44:33 -0800

    Set PRODUCT_AAPT_PREBUILT_DPI.
    The build system use both PRODUCT_AAPT_PREF_CONFIG and
    PRODUCT_AAPT_PREBUILT_DPI to select prebuilt dpi-specific apks.
    The first match takes precedence.
    
    Bug: 18388705
    Change-Id: Iba98af754c3397ddbb27287244db086cb4c43aa6

--- device-common.mk
+++ device-common.mk
@@ -36,8 +36,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # currently contain all of the bitmaps at xhdpi density so
 # we do this little trick to fall back to the hdpi version
 # if the xhdpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
+# A list of dpis to select prebuilt apk, in precedence order.
+PRODUCT_AAPT_PREBUILT_DPI := hdpi
 
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
