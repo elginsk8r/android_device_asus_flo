95626a0c958f85863ac8beda132e2f505504f56b
Amit Pundir <amit.pundir@linaro.org>
2016-04-28 12:17:32 +0530

    flo: enable android configfs gadgets adb, mtp and ptp
    Android composite gadget driver is no longer supported on newer > v3.10
    kernels. This patch removes the Android composite cadget configurations
    and switch over to ConfigFS gadget implementations instead.
    
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Change-Id: I7edb809bc4057a260629738ee2756eca443ce66b

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -42,7 +42,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := init.flo.usb.rc
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
+ifeq ($(PRODUCT_KERNEL_VERSION),mainline)
+LOCAL_SRC_FILES := etc/init.flo_mainline.usb.rc
+else
 LOCAL_SRC_FILES := etc/init.flo.usb.rc
+endif
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
