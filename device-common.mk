13557cabf32dbf77c806bdc10b5177fd2ab609ec
Miao Wang <miaowang@google.com>
2017-05-24 15:34:15 -0700

    Addd RenderScript default implementation to Hammerhead device.mk
    Bug: 37035983
    
      - OVERRIDE_RS_DRIVER is moved from libRS_internal to the default
        implmentation. In order for GPU driver to work on legacy devices,
        the default implementation needs to be part of PRODUCT_PACKAGES.
    
    Test: mm
    Test: CTS tests pass on Fugu, and confirm GPU driver is working.
    Change-Id: I122c18be7a77449bf17c92d6b1c74c685433f249

--- device-common.mk
+++ device-common.mk
@@ -173,6 +173,10 @@ PRODUCT_PACKAGES += \
 	android.hardware.graphics.composer@2.1-impl \
 	android.hardware.graphics.mapper@2.0-impl
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+	android.hardware.renderscript@1.0-impl
+
 PRODUCT_PACKAGES += \
 	audio.primary.msm8960 \
 	audio.a2dp.default \
