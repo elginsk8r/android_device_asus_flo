49bf961486a30e3b44f0be0b1a66e54c41b62f28
Martijn Coenen <maco@google.com>
2017-08-27 00:18:16 +0200

    Add keymaster implementations to flo
    Test: Fugu boots again
    Bug: 34609804
    Change-Id: I63d0783742a0f94e55c7c8b69ab5bb57e59efd84

--- device-common.mk
+++ device-common.mk
@@ -281,3 +281,7 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, frameworks/native/build/tablet-7in-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
+
+# Keymaster HAL
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
