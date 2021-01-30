ee3dd035d8a3a067b450b5f399612d1bcff9318b
Nick Reuter <nreuter85@gmail.com>
2021-01-29 18:33:10 -0600

    Revert "flo: Build Trust HAL"
    This reverts commit 1a32d0d2c0500e2bf2dbbfc16a0b05894429646f.
    
    Change-Id: I6e303e1fa55d8da5c025acd671c3fa44f94d33a0

--- device-common.mk
+++ device-common.mk
@@ -150,10 +150,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     lights.msm8960
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
